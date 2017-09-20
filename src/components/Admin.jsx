f94cb10d194e451a7123401c04cdc63540ff3e50
Brittany Kerr and Michelle Poterek <brittany@>
2017-09-20 10:07:37 -0700

    write in routing and create admin with ticketlist showing

---
+++ src/components/Admin.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import TicketList from './TicketList';
+import {connect} from 'react-redux'
+
+class Admin extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Admin page!</h3>
+        <TicketList ticketList = {this.props.adminMasterTicketList}/>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    adminMasterTicketList : state
+  }
+}
+
+export default connect(mapStateToProps)(Admin);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
-function Header(){
+function Header(props){
   return (
+  <div>
     <h1>Help Queue!</h1>
+    <Link to="/">Home</Link> | <Link to="/admin">Admin</Link>
+  </div>
   );
 }
 
