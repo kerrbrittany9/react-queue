5735e52b924eee990e92069c3414cc0a45e815e9
Brittany Kerr <kerrbrittany9@gmail.com>
2017-09-24 09:45:43 -0700

    add middleware

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
 import App from "./components/App";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducers/ticket-list-reducer";
 import { Provider } from "react-redux";
 import { HashRouter } from 'react-router-dom';
+import middleware from './middleware/middleware-logger';
+import persistDataLocally from './middleware/persist-local-storage-data';
 
-const store = createStore(reducer);
+let retrievedState;
+try {
+    retrievedState = localStorage.getItem("reduxStore");
+  if (retrievedState === null){
+    retrievedState = [];
+  }
+  retrievedState = JSON.parse(retrievedState);
+} catch (err){
+  retrievedState = [];
+}
+
+const store = createStore(reducer, applyMiddleware(middlewareLogger, persistDataLocally));
 
 
 ReactDOM.render(
